4ab8d7f89b85fa45a403e50af4807f84c4e984ab
bormannb <99260828+bormannb@users.noreply.github.com>
2022-05-10 15:36:18 +0200

    Add files via upload

--- src/main/java/com/wildcodeschool/wildandwizard/entity/Wizard.java
+++ src/main/java/com/wildcodeschool/wildandwizard/entity/Wizard.java
@@ -1,10 +1,15 @@
 package com.wildcodeschool.wildandwizard.entity;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import java.sql.Date;
+import java.util.Locale.Category;
 
 @Entity
 public class Wizard {
@@ -18,6 +23,11 @@ public class Wizard {
     private String birthPlace;
     private String biography;
     private boolean muggle;
+
+    @ManyToOne (fetch=FetchType.LAZY, cascade = CascadeType.REFRESH, optional = false)
+    @JoinColumn(name="school_id", nullable = false)
+
+    private School school;
 
     public Wizard() {
     }
@@ -77,4 +87,21 @@ public class Wizard {
     public void setMuggle(boolean muggle) {
         this.muggle = muggle;
     }
+    
+        //------
+
+    public School getSchool(){
+    return school;
+
+    }
+
+    public void setSchool(School school){
+        this.school = school;
+    }
+
+    
+
+    
+
+    
 }
